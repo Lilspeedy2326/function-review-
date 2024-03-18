f83d57a6f45b287a07556d391f38d4b152f345c3
Lilspeedy2326 <146358585+Lilspeedy2326@users.noreply.github.com>
2024-03-18 09:44:09 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def calculate_distance(speed, time):
+    distance = speed * time
+    print(distance, "kmh")
+    return distance
+
+calculate_distance(60,3)
+
+def calculate_area_of_rectangle(length,width):
+    area = length * width
+    print(area)
+    return area
+
+calculate_area_of_rectangle(10,5)
