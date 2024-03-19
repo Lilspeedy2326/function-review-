10c935eb5d1674e2046b1a5fa46818015963e08d
Lilspeedy2326 <146358585+Lilspeedy2326@users.noreply.github.com>
2024-03-19 09:30:56 -0600

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+def calculate_health(health, damage, armor):
+    health = armor * damage - health
+    print(health)
+    return health
+
+calculate_health(100, 10, .5)
+
+def average_of_five(one,two,three,four,five):
+    five = one+two+three+four+five
+    print(five)
+    
+average_of_five(10,20,30,40,50)
